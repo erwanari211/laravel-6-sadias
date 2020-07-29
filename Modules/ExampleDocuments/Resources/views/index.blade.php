8e5c2bbae58c32f7eed5fdae5db506912f1626db
Erwan <erwan.ari211@gmail.com>
2020-07-29 15:43:59 +0700

    add module ExampleDocuments

---
+++ Modules/ExampleDocuments/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('exampledocuments::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('exampledocuments.name') !!}
+    </p>
+@endsection
