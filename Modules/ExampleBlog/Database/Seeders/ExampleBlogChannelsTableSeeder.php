858731f81c7207be5c9ba2ae73b0a0d34f3ad8ee
Erwan <erwan.ari211@gmail.com>
2020-07-30 15:31:52 +0700

    fix ExampleBlog seeder

--- Modules/ExampleBlog/Database/Seeders/ExampleBlogChannelsTableSeeder.php
+++ Modules/ExampleBlog/Database/Seeders/ExampleBlogChannelsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Modules\ExampleBlog\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Modules\ExampleBlog\Entities\ExampleBlogChannel as Channel;
+use App\User;
 
 class ExampleBlogChannelsTableSeeder extends Seeder
 {
@@ -17,6 +18,13 @@ class ExampleBlogChannelsTableSeeder extends Seeder
     {
         Model::unguard();
 
-        create(Channel::class, [], 10);
+        for ($i=1; $i <= 10; $i++) {
+            $user = create(User::class, [
+                'email' => 'channel_owner'.$i.'@app.com',
+                'password' => bcrypt(12345678),
+            ]);
+            create(Channel::class, ['owner_id' => $user->id]);
+        }
+
     }
 }
