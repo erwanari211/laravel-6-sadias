6d745dfdbedce5b668f055d9b1529c8eebd719c2
Erwan <erwan.ari211@gmail.com>
2020-08-28 19:34:15 +0700

    fix helpers
    add upload_file and delete_file function

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -6,3 +6,29 @@ if (!function_exists('example_function')) {
         return $string;
     }
 }
+
+if (!function_exists('upload_file')) {
+    function upload_file($file, $directory = 'uploads', $newFileName = null)
+    {
+        $originalName = $file->getClientOriginalName();
+        $filename = $newFileName ? $newFileName : pathinfo($originalName, PATHINFO_FILENAME);
+        $ext = $file->getClientOriginalExtension();
+        $newFileName = Str::slug($filename).'-'.Str::random(8).'-'.time().'.'.$ext;
+        $file->move($directory, $newFileName);
+        $filepath = $directory.'/'.$newFileName;
+
+        return $filepath;
+    }
+}
+
+if (!function_exists('delete_file')) {
+    function delete_file($filepath = '')
+    {
+        $isDeleted = false;
+        if (File::exists($filepath)) {
+            $isDeleted = File::delete($filepath);
+        }
+
+        return $isDeleted;
+    }
+}
